7b52b5afd7d43fc2d73c75fa3ca9f1e511327026
George Revkov <compresce@gmail.com>
2019-12-09 01:00:50 -0800

    Mostly fixes. Added missing imports. Moved a couple methods to misc.py. Separated transforms into one for output and one for the mask

--- src/rle_handler.py
+++ src/rle_handler.py
@@ -1,8 +1,10 @@
-class RLEHandler(): 
+import numpy as np
+
+class RLEHandler():
     """
-    Take the individual ship masks and 
+    Take the individual ship masks and
     create a single mask array for all ships
-    
+
     """
     def __init__(self, encoded_masks):
         self.encoded_masks = encoded_masks
@@ -10,10 +12,10 @@ class RLEHandler():
     def rle_decode(self, mask_rle, shape=(768, 768)):
         """
         mask_rle: run-length as string formated (start length)
-        shape: (height,width) of array to return 
+        shape: (height,width) of array to return
         Returns numpy array, 1 - mask, 0 - background
         """
-        
+
         s = mask_rle.split()
         starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
         starts -= 1
@@ -22,11 +24,11 @@ class RLEHandler():
         for lo, hi in zip(starts, ends):
             img[lo:hi] = 1
         return img.reshape(shape).T  # Needed to align to RLE direction
-    
+
     def masks_as_image(self):
         """ Create a single mask array for all ships. """
         all_masks = np.zeros((768, 768), dtype = np.int16)
-        
+
         for mask in self.encoded_masks:
             if isinstance(mask, str):
                 all_masks += self.rle_decode(mask)
